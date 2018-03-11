fd62b61c16b93127ccebb98cbe43111bd2b9e802
Steven M. Fortune <sfortune@live.com>
2018-03-11 17:22:38 -0500

    Inital Commit.

--- File Locker/Program.cs
+++ File Locker/Program.cs
@@ -16,7 +16,7 @@ namespace File_Locker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
--- File Locker/frmMain.Designer.cs
+++ File Locker/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace File_Locker
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(357, 282);
+            this.Name = "frmMain";
+            this.Text = "File Locker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
